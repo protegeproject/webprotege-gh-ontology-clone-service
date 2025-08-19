92a79d33d57a8f7db00adc2c0b2fcbdd16e28ea8
Josef Hardi <josef.hardi@gmail.com>
2025-08-19 14:19:58 -0700

    API changes to use GitHubRepository directly instead of CommitNavigator

--- src/main/java/edu/stanford/protege/github/cloneservice/ExampleUsage.java
+++ src/main/java/edu/stanford/protege/github/cloneservice/ExampleUsage.java
@@ -1,10 +1,10 @@
 package edu.stanford.protege.github.cloneservice;
 
+import edu.stanford.protege.commitnavigator.GitHubRepository;
 import edu.stanford.protege.commitnavigator.GitHubRepositoryBuilder;
 import edu.stanford.protege.commitnavigator.exceptions.GitHubNavigatorException;
-import edu.stanford.protege.commitnavigator.services.CommitNavigator;
 import edu.stanford.protege.github.cloneservice.exception.OntologyComparisonException;
-import edu.stanford.protege.github.cloneservice.utils.DifferenceCalculator;
+import edu.stanford.protege.github.cloneservice.utils.OntologyDifferenceCalculator;
 import edu.stanford.protege.github.cloneservice.utils.OntologyHistoryAnalyzer;
 import edu.stanford.protege.github.cloneservice.utils.OntologyLoader;
 
@@ -12,32 +12,33 @@ public class ExampleUsage {
 
     public static void main(String[] args) {
         try {
-            var navigator = getCommitNavigator();
+            var repository = getGitHubRepository("https://github.com/protegeteam/grocery-ontology");
             var ontologyLoader = new OntologyLoader();
-            var differenceCalculator = new DifferenceCalculator();
-            var ontologyHistoryAnalyzer = new OntologyHistoryAnalyzer(ontologyLoader, differenceCalculator);
-            var results = ontologyHistoryAnalyzer.getCommitHistory("ro-full.owl", navigator);
+            var ontologyDifferenceCalculator = new OntologyDifferenceCalculator();
+            var ontologyHistoryAnalyzer = new OntologyHistoryAnalyzer(ontologyLoader, ontologyDifferenceCalculator);
+            var results = ontologyHistoryAnalyzer.getCommitHistory("grocery.owl", repository);
             System.out.println("Ontology History Analysis Results:");
             results.forEach((commit) -> {
                 var commitMetadata = commit.commitMetadata();
                 System.out.println("Commit: " + commitMetadata.commitHash() + " by " + commitMetadata.committerUsername() + " on " + commitMetadata.commitDate());
+                System.out.println("Message: " + commitMetadata.commitMessage().trim());
                 System.out.println("Axiom changes:");
                 commit.axiomChanges().forEach(
                         (axiomChange) -> System.out.println("- " + axiomChange.operationType() + " " + axiomChange.axiom()));
+                System.out.println();
             });
-            System.out.println("\n");
         } catch (GitHubNavigatorException | OntologyComparisonException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static CommitNavigator getCommitNavigator() throws GitHubNavigatorException {
+    private static GitHubRepository getGitHubRepository(String repositoryUrl) throws GitHubNavigatorException {
         var repository = GitHubRepositoryBuilder
-                .forRepository("https://github.com/oborel/obo-relations")
+                .forRepository(repositoryUrl)
                 .branch("master")
                 .fileFilters("*.owl", "*.rdf", "*.ttl")
                 .build();
         repository.initialize();
-        return repository.getCommitNavigator();
+        return repository;
     }
 }
