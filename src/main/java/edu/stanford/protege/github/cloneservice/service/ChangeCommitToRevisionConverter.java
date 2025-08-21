5d919230dcdbce6674fee6b560d4daefbd0ad513
Josef Hardi <josef.hardi@gmail.com>
2025-08-21 10:10:06 -0700

    Add method to convert an ontology commit from a GitHub repository into a WebProtege revision

---
+++ src/main/java/edu/stanford/protege/github/cloneservice/service/ChangeCommitToRevisionConverter.java
@@ -0,0 +1,51 @@
+package edu.stanford.protege.github.cloneservice.service;
+
+import com.google.common.collect.ImmutableList;
+import edu.stanford.protege.github.cloneservice.model.AxiomChange;
+import edu.stanford.protege.github.cloneservice.model.OntologyCommitChange;
+import edu.stanford.protege.webprotege.change.AddAxiomChange;
+import edu.stanford.protege.webprotege.change.OntologyChange;
+import edu.stanford.protege.webprotege.change.RemoveAxiomChange;
+import edu.stanford.protege.webprotege.common.UserId;
+import edu.stanford.protege.webprotege.revision.Revision;
+import edu.stanford.protege.webprotege.revision.RevisionNumber;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Component
+public class ChangeCommitToRevisionConverter {
+
+    private final AtomicLong orderNumber = new AtomicLong(1);
+
+    /**
+     * Converts an ontology commit change from a GitHub repository into a WebProtege revision.
+     * 
+     * <p>This method transforms git commit information including metadata and axiom changes
+     * into a format suitable for WebProtege's revision system. Each commit is assigned an
+     * incrementing revision number to maintain chronological ordering.</p>
+     * 
+     * @param ontologyCommitChange the commit change containing git metadata and axiom modifications
+     * @return a {@link Revision} representing the commit as a WebProtege revision with user ID,
+     *         revision number, ontology changes, timestamp, and commit message
+     */
+    public Revision convert(OntologyCommitChange ontologyCommitChange) {
+        var commitMetadata = ontologyCommitChange.commitMetadata();
+        var userId = UserId.valueOf(commitMetadata.committerUsername());
+        var revisionNumber = RevisionNumber.getRevisionNumber(orderNumber.getAndIncrement());
+        var ontologyChanges = ontologyCommitChange.axiomChanges().stream()
+                .map(this::convertToOntologyChange)
+                .collect(ImmutableList.toImmutableList());
+        var commitTimestamp = commitMetadata.commitDate().toEpochMilli();
+        var commitMessage = commitMetadata.commitMessage();
+        return new Revision(userId, revisionNumber, ontologyChanges, commitTimestamp, commitMessage);
+    }
+
+    private OntologyChange convertToOntologyChange(AxiomChange axiomChange) {
+        var operation = axiomChange.operationType();
+        return switch (operation) {
+            case ADD -> new AddAxiomChange(axiomChange.ontologyID(), axiomChange.axiom());
+            case REMOVE -> new RemoveAxiomChange(axiomChange.ontologyID(), axiomChange.axiom());
+        };
+    }
+}
