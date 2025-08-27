7a08fd41e1ee02016a84a0a8eec7a5d6e97bced2
Josef Hardi <josef.hardi@gmail.com>
2025-08-26 20:01:29 -0700

    Create defensive copy to prevent external mutation

--- src/main/java/edu/stanford/protege/github/cloneservice/model/OntologyCommitChange.java
+++ src/main/java/edu/stanford/protege/github/cloneservice/model/OntologyCommitChange.java
@@ -1,6 +1,9 @@
 package edu.stanford.protege.github.cloneservice.model;
 
+import com.google.common.collect.ImmutableList;
 import edu.stanford.protege.commitnavigator.model.CommitMetadata;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import javax.annotation.Nonnull;
@@ -15,5 +18,7 @@ public record OntologyCommitChange(
   public OntologyCommitChange {
     Objects.requireNonNull(axiomChanges, "axiomChanges cannot be null");
     Objects.requireNonNull(commitMetadata, "commitMetadata cannot be null");
+    // Create defensive copy to prevent external mutation
+    axiomChanges = ImmutableList.copyOf(axiomChanges);
   }
 }
